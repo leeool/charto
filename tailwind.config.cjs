3e982e6d8a002ae05ba68be7aa0a0533b346e026
leeool <lgonsalez02@gmail.com>
2023-01-21 17:21:57 -0300

    💄 feat: add tailwind color palettes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
-    extend: {}
+    extend: {
+      textColor: {
+        smoke: "#efefef",
+        carbon: {
+          100: "#555"
+        }
+      },
+
+      backgroundColor: {
+        carbon: {
+          100: "#555",
+          200: "#444",
+          300: "#2f2f2f",
+          400: "#2a2a2a"
+        },
+        smoke: "#efefef"
+      }
+    }
   },
   plugins: []
 }
