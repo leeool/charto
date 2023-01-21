bd54be968eacd0ebe896d908e3a501704e2f44ba
leeool <lgonsalez02@gmail.com>
2023-01-21 15:46:11 -0300

    :rocket: first deploy

---
+++ src/main.tsx
@@ -0,0 +1,26 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import "./style.css"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/favoritos"
+      },
+      {
+        path: "/descobrir"
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+)
