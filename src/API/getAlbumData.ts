3f89ffd524bd7714a1206c46c5f06fe6acff25c7
leeool <lgonsalez02@gmail.com>
2023-01-26 01:18:51 -0300

    🔒️ feat: improve error handler

--- src/API/getAlbumData.ts
+++ src/API/getAlbumData.ts
@@ -4,6 +4,7 @@ import getAuthorization from "./getAuthorization"
 export const fetchData = async (query?: string) => {
   try {
     const auth = await getAuthorization()
+    if (!auth.access_token) throw new Error("Token Error")
     const data = await getData.get("/search", {
       params: {
         q: "war",
@@ -13,31 +14,27 @@ export const fetchData = async (query?: string) => {
         Authorization: `Bearer ${auth.access_token}`
       }
     })
+    if (data.statusText !== "OK") throw new Error(data.statusText)
     return data.data.albums.items
   } catch (e) {
-    if (e instanceof Error) {
-      console.error(`${e.name}: ${e.message}`)
-    }
+    if (e instanceof Error) console.error(`${e.name}: ${e.message}`)
     return null
   }
 }
 
 const getAlbum = (data: IFetchData[]): IAlbumData[] | null => {
-  if (data) {
-    return data.map((album) => {
-      return {
-        id: album.id,
-        name: album.name,
-        artists: album.artists,
-        cover: album.images,
-        spotifyLink: album.external_urls.spotify,
-        releaseDate: album.release_date,
-        type: album.type
-      }
-    })
-  } else {
-    return null
-  }
+  if (!data) return null
+  return data.map((album) => {
+    return {
+      id: album.id,
+      name: album.name,
+      artists: album.artists,
+      cover: album.images,
+      spotifyLink: album.external_urls.spotify,
+      releaseDate: album.release_date,
+      type: album.type
+    }
+  })
 }
 
 export default getAlbum
