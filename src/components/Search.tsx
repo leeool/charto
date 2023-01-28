6c8a4d8581e64a19ae7ff298bd215f9d8a43aa62
leeool <lgonsalez02@gmail.com>
2023-01-28 14:56:14 -0300

    💄 feat: improve search icon

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -18,18 +18,18 @@ const Search = ({ value, onChange, placeholder }: ISearch) => {
       />
       <button className="bg-carbon-400 px-2 py-1 hover:bg-carbon-200 flex items-center">
         <svg
-          width="20"
-          height="20"
-          viewBox="0 0 15 15"
-          fill="rgb(239 239 239)"
           xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={2}
+          stroke="currentColor"
+          className="w-6 h-6 stroke-smoke-100"
         >
           <path
-            d="M10 6.5C10 8.433 8.433 10 6.5 10C4.567 10 3 8.433 3 6.5C3 4.567 4.567 3 6.5 3C8.433 3 10 4.567 10 6.5ZM9.30884 10.0159C8.53901 10.6318 7.56251 11 6.5 11C4.01472 11 2 8.98528 2 6.5C2 4.01472 4.01472 2 6.5 2C8.98528 2 11 4.01472 11 6.5C11 7.56251 10.6318 8.53901 10.0159 9.30884L12.8536 12.1464C13.0488 12.3417 13.0488 12.6583 12.8536 12.8536C12.6583 13.0488 12.3417 13.0488 12.1464 12.8536L9.30884 10.0159Z"
-            fill="currentColor"
-            fill-rule="evenodd"
-            clip-rule="evenodd"
-          ></path>
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+          />
         </svg>
       </button>
     </div>
